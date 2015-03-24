5c47dea33f1bf341648e3180c3f9aae5cf2dd406
constantinoschristoforou <constantinos23@gmail.com>
2015-03-24 14:04:09 +0200

    Convert money amount to float

--- src/Currency/CurrencyConverter.php
+++ src/Currency/CurrencyConverter.php
@@ -61,7 +61,7 @@ class CurrencyConverter
     {
         $rate = self::getCurrencyRate($action, $currencyCode);
 
-
+        $amount=self::getFloatAmount($amount);
 
         if (strtolower($action) == "sell") {
 
@@ -128,5 +128,18 @@ class CurrencyConverter
      public static  function getRates()
     {
         return self::$rates;
+    }
+
+    public static function getFloatAmount($money)
+    {
+        $cleanString = preg_replace('/([^0-9\.,])/i', '', $money);
+        $onlyNumbersString = preg_replace('/([^0-9])/i', '', $money);
+
+        $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
+
+        $stringWithCommaOrDot = preg_replace('/([,\.])/', '', $cleanString, $separatorsCountToBeErased);
+        $removedThousendSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot);
+
+        return (float) str_replace(',', '.', $removedThousendSeparator);
     }
 }
